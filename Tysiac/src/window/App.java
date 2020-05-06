b7e7965f7fc5eeae6de24f64e6b012e36c69113f
krzysztof <krzysztof@192.168.1.24>
2020-05-06 21:51:28 +0200

    poprawki

--- Tysiac/src/window/App.java
+++ Tysiac/src/window/App.java
@@ -1,11 +1,36 @@
 package window;
 
-import graphic.MyFrame;;
+import java.io.IOException;
 
-public class App {
+import game.Deck;
+import game.Rules;
+
+public class App extends Rules{
 	
-	public static void main(String[] args) {
-		MyFrame frame = new MyFrame();
+	public static void main(String[] args) throws IOException {
+		Deck deck = new Deck();
+		
+		deck.resetDeck();
+		deck.dealCards();
+		
+		//System.out.println(deck.firstPlayerHand.get(0).showCard());
+		//System.out.println(deck.firstPlayerHand.get(3).showCard());
+
+		
+		int a = 1;
+		int b = 6;
+		
+		
+		if(compareCards(deck.firstPlayerHand.get(a), deck.firstPlayerHand.get(b))) {
+			System.out.println("karta '" +deck.firstPlayerHand.get(a).showCard() +"'" +" wieksza od karty '" 
+								+deck.firstPlayerHand.get(b).showCard() +"'");
+		}
+		else {
+			System.out.println("karta '" +deck.firstPlayerHand.get(a).showCard() +"'" +" mniejsza od karty '" 
+					+deck.firstPlayerHand.get(b).showCard() +"'");
+		}
+			
+
 
 
 	}
